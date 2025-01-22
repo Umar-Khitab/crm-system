2fb7e8dfa50b99477b84b3e73e8a88453574597a
Umar-Khitab <khitab.khan007@gmail.com>
2025-01-22 16:12:08 +0500

    style ui of lead list page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 
 import './App.css'
 import LeadsList from './pages/LeadsList'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Router>
       <LeadsList/>
+
+      </Router>
     </>
   )
 }
--- client/src/pages/LeadsList.jsx
+++ client/src/pages/LeadsList.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "../components/ui/button"
+import { Box, Card,Heading } from "@chakra-ui/react"
+import { Avatar } from "../components/ui/avatar"
 import axios from "axios";
-
+import { Link } from "react-router-dom";
 
 const LeadsList = () => {
   const [leads, setLeads] = useState([]);
@@ -38,10 +41,13 @@ const LeadsList = () => {
 
   return (
     <div>
-      <h1>Leads List</h1>
-      <div>
-        <label>Filter by Status: </label>
-        <select onChange={(e) => setStatusFilter(e.target.value)} value={statusFilter}>
+      <Heading m="5" size={"3xl"}>Leads List</Heading>
+        <Box display="flex" alignItems={"center"}>
+        <Button  ml="5" mb="3" size="sm" px="4"> <Link to="/add">Add Lead</Link></Button>
+         <Box ml={"2"}>
+         <div className="select_box_container">
+        <label className="select_box">Filter by Status: </label>
+        <select className="select_input" onChange={(e) => setStatusFilter(e.target.value)} value={statusFilter}>
           <option value="All">All</option>
           <option value="New">New</option>
           <option value="Contacted">Contacted</option>
@@ -50,20 +56,32 @@ const LeadsList = () => {
           <option value="Closed">Closed</option>
         </select>
       </div>
+         </Box>
+        </Box>
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
-      <ul>
+      <Box as="ul" m="5"> 
         {filteredLeads.map((lead) => (
-          <li key={lead._id}>
-            <h2>{lead.name}</h2>
+              <Card.Root   mt="3" width="320px">
+              <Card.Body gap="2">
+           
+                <Card.Title mt="2">{lead.name}</Card.Title>
+                <Card.Description as={"li"} key={lead._id}>
             <p>Email: {lead.email}</p>
             <p>Phone: {lead.phone}</p>
             <p>Status: {lead.status}</p>
             <p>Created At: {new Date(lead.createdAt).toLocaleString()}</p>
-            <button onClick={() => handleDelete(lead._id)}>Delete</button>
-          </li>
+
+                </Card.Description>
+              </Card.Body>
+              <Card.Footer justifyContent="flex-end">
+            <Link to={`/edit/${lead._id}`}><Button size="xs" colorPalette="cyan" variant="outline">Edit</Button></Link>
+            <Button variant="subtle" size="xs" colorPalette="red" onClick={() => handleDelete(lead._id)}>Delete</Button>
+              </Card.Footer>
+            </Card.Root>
+        
         ))}
-      </ul>
+      </Box>
     </div>
   );
 };
