f8613f3a5d43cba0ba5d730f5de0ff77dc670fa6
Umar-Khitab <khitab.khan007@gmail.com>
2025-01-22 14:32:39 +0500

    allow cors policy

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-
+const cors = require("cors")
 dotenv.config();
 
 const app = express();
+app.use(cors())
 const PORT = process.env.PORT || 5000;
 
 // Middleware
